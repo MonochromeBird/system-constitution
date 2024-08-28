f1b41383ea5eb3eb7819e077fd02513ac5519a1d
Niki <nightfallout@proton.me>
2024-08-28 23:54:14 +0000

    config.nix: surrounded absolutePath with quotes

--- config.nix
+++ config.nix
@@ -1,5 +1,8 @@
 {
-	absolutePath = /conf;
+	# Path must be in quotes! Otherwise, Nix will turn it
+	# into a path to a /nix/store copy of the configuration.
+	absolutePath = "/conf";
+
 	home.stateVersion = "24.05";
 	system.stateVersion = "24.05";
 }
