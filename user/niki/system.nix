27dacc9598af6ac27d47de6f3265b5b4b3a3b3c3
Niki <nightfallout@proton.me>
2024-08-28 23:20:30 +0000

    user [niki]: syncthing

--- user/niki/system.nix
+++ user/niki/system.nix
@@ -10,10 +10,28 @@
 	];
 
 	system = {
-		services.xserver = {
-			enable = true;
-			displayManager.gdm.enable = true;
-			windowManager.awesome.enable = true;
+		services = {
+			xserver = {
+				enable = true;
+				displayManager.gdm.enable = true;
+				windowManager.awesome.enable = true;
+				
+				xkb = {
+					layout = "us,br";
+					options = "grp:alt_caps_toggle";
+				};
+			};
+
+			syncthing = {
+				enable = true;
+
+				openDefaultPorts = true;
+				settings.options.urAccepted = -1;
+
+				user = "niki";
+				dataDir = "/home/niki/.local/share/syncthing";
+				configDir = "/home/niki/.local/share/syncthing/config";
+			};
 		};
 	};
 }
