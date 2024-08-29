3846f06c579f263dcc19628130c86ab60dce394f
anonymalek <alekthesixth@protonmail.com>
2024-08-29 17:53:20 +0000

    user [alek]: sddm on system.nix instead of plasma/system.nix

--- user/alek/config/desktop/plasma/system.nix
+++ user/alek/config/desktop/plasma/system.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 {
 	services.desktopManager.plasma6.enable = true;
-	services.displayManager.sddm.enable = true;
 	services.xserver.enable = true;
 
 	environment.systemPackages = with pkgs; [
--- user/alek/system.nix
+++ user/alek/system.nix
@@ -8,6 +8,8 @@
 	systemUsers = [ "alek" "niki" ];
 
 	system = {
+		services.displayManager.sddm.enable = true;
+		
 		services.xserver.xkb.layout  = "br";
 		services.xserver.xkb.options = "grp:alt_caps_toggle";
 	};
