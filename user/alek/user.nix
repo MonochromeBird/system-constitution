d193a06c52e314b4c9d096063a05dcf66a1752d5
anonymalek <alekthesixth@protonmail.com>
2024-08-29 19:52:47 +0000

    user [alek]: zoxide (cryaboutit)

--- user/alek/user.nix
+++ user/alek/user.nix
@@ -45,7 +45,7 @@
 		imports = [ ./config/desktop/plasma/home.nix ];
 
 		programs.zoxide = { enable = true; enableFishIntegration = true; } ;
-		programs.fish.enable = true;
+		programs.fish = { enable = true; interactiveShellInit = "zoxide init fish | source"; };
 		
 		services.unclutter = {
 			enable = true;
