560840f57a3db4bbbabc0841b96c7dbb7f5ad5c5
anonymalek <alekthesixth@protonmail.com>
2024-08-29 19:58:31 +0000

    user [alek]: "z"

--- user/alek/user.nix
+++ user/alek/user.nix
@@ -45,7 +45,7 @@
 		imports = [ ./config/desktop/plasma/home.nix ];
 
 		programs.zoxide = { enable = true; enableFishIntegration = true; } ;
-		programs.fish = { enable = true; interactiveShellInit = "zoxide init fish | source"; };
+		programs.fish = { enable = true; interactiveShellInit = "(zoxide init fish | source); alias cd=z"; };
 		
 		services.unclutter = {
 			enable = true;
