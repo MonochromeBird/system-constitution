8377c1dc881bc3d12af694696ed3bd0ed3960af5
Niki <nightfallout@proton.me>
2024-08-31 17:49:11 +0000

    user [niki]: moved out. bye!

--- user/niki/config/emacs/emacs.el
+++
@@ -1,3 +0,0 @@
-(load-file "~/.config/emacs/init.el")
-(setq custom-file "~/.config/emacs/init.el")
-
--- user/niki/config/link.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname $(realpath "$0"))
-
-mkdir -p ~/.config
-mkdir -p ~/.config/qutebrowser
-
-sed $BASEDIR/links.csv -e "s/,/ \~\//g" -e "s#.*#ln -sn -T $BASEDIR\/&#g" | sh
