c91543b6b8d72a4b7aafae8d85d6dc41b5cf14a0
Niki <nightfallout@proton.me>
2024-09-09 16:54:10 +0000

    system [sandbox/sandbox-exec-directory.sh]: added ability to mark entire directory as mutable and home-relative mount directories

--- system/sources/sandbox-exec-directory.sh
+++ system/sources/sandbox-exec-directory.sh
@@ -5,47 +5,55 @@ Performs a sandboxed action on a given directory, while only
 mirroring changes to certain specified subdirectories and
 discarding everything else.
 
+The given directory is copied over (unless the mutable directories
+argument is "." or "./") and the mutable subdirectories are mounted.
+
 The script makes use of 'mount --bind', so recursively deleting
 files in the specified sandbox home may damage the mirrored
 subdirectories. All directories are umounted at the end of the
-script."
+script, assuming the process manages to survive and the mount
+points are not busy."
 
-if [ $# != 5 ]; then
+if [ $# != 7 ]; then
 	printf "$MSG\n\n"
 
 	printf "Invalid usage. Syntax:"
-	printf " sandbox-exec-directory <target directory>"
-	printf " <sandbox namespace> <directory within sandbox home>"
-	printf " <comma separated mutable directory list or empty"
-	printf " string> <command>\n"
+	printf " sandbox-exec-directory"
+	printf " <target directory>"
+	printf " <sandbox home directory>"
+	printf " <directory within sandbox home>"
+	printf " <comma separated mutable directory list or empty string>"
+	printf " <comma separated mutable directory list relative to home or empty string>"
+	printf " <firejail extra args or empty string>"
+	printf " <command>\n"
 	exit 1
 fi
 
-NAMESPACE=$2
-SANDBOX_HOME=~/.sandbox/$NAMESPACE
+SANDBOX_HOME=$2
 DIRECTORY_WITHIN=$3
 MUTABLE=$4
-
-COMMAND=$5
+MUTABLE_RELATIVE_TO_HOME=$5
+FIREJAIL_EXTRA_ARGS="$6"
+COMMAND=$7
 
 if [ "$COMMAND" = "" ]; then
 	COMMAND=ls
 fi
 
-FIREJAIL_ARGS="--quiet --blacklist=/conf --novideo --name=$NAMESPACE"
+FIREJAIL_ARGS="--quiet --blacklist=/conf --novideo --private-tmp --noblacklist=~/.gradle $FIREJAIL_EXTRA_ARGS"
 
 DIR=$1
-
-for i in $(echo "$MUTABLE" | sed "s/,/\n/g")
-do
-	mkdir -p $DIR/$i
-done
 
 Umount()
 {
 	for i in $(echo "$MUTABLE" | sed "s/,/\n/g")
 	do
 		sudo umount $SANDBOX_HOME/$DIRECTORY_WITHIN/$i || true
+	done
+
+	for i in $(echo "$MUTABLE_RELATIVE_TO_HOME" | sed "s/,/\n/g")
+	do
+		sudo umount $SANDBOX_HOME/$i || true
 	done
 }
 
@@ -55,6 +63,37 @@ Mount()
 	do
 		sudo mount --bind $DIR/$i $SANDBOX_HOME/$DIRECTORY_WITHIN/$i
 	done
+
+	for i in $(echo "$MUTABLE_RELATIVE_TO_HOME" | sed "s/,/\n/g")
+	do
+		sudo mount --bind ~/$i $SANDBOX_HOME/$i
+	done
+}
+
+GetMounted()
+{
+	mountpoint -q $SANDBOX_HOME/$DIRECTORY_WITHIN && printf "$SANDBOX_HOME/$DIRECTORY_WITHIN\n"
+
+	for i in $(echo "$MUTABLE" | sed "s/,/\n/g")
+	do
+		mountpoint -q $SANDBOX_HOME/$DIRECTORY_WITHIN/$i && printf "$SANDBOX_HOME/$DIRECTORY_WITHIN/$i\n"
+	done
+
+	for i in $(echo "$MUTABLE_RELATIVE_TO_HOME" | sed "s/,/\n/g")
+	do
+		mountpoint -q $SANDBOX_HOME/$i && printf "$SANDBOX_HOME/$i\n"
+	done
+}
+
+AbortIfMounted()
+{
+	MOUNTED=$(GetMounted)
+
+	if [ $(printf "$MOUNTED" | wc -w) != "0" ]; then
+		echo "Some directories are still mounted. Aborting. Proceed with caution when operating on $SANDBOX_HOME."
+		printf "Mounted directories:\n$MOUNTED\n"
+		exit 1
+	fi
 }
 
 Mkdirs()
@@ -62,10 +101,18 @@ Mkdirs()
 	for i in $(echo "$MUTABLE" | sed "s/,/\n/g")
 	do
 		mkdir -p $SANDBOX_HOME/$DIRECTORY_WITHIN/$i
+		mkdir -p $DIR/$i
+	done
+
+	for i in $(echo "$MUTABLE_RELATIVE_TO_HOME" | sed "s/,/\n/g")
+	do
+		mkdir -p $SANDBOX_HOME/$i
+		mkdir -p ~/$i
 	done
 }
 
 Umount
+AbortIfMounted
 
 rm -rf $SANDBOX_HOME/$DIRECTORY_WITHIN
 
@@ -76,13 +123,21 @@ do
 	printf " --exclude=$DIR/$i "
 done)
 
-rsync -r $DIR/* $SANDBOX_HOME/$DIRECTORY_WITHIN $EXCLUDE_PARAMS
+if [ "$EXCLUDE_PARAMS" != "." ] && [ "$EXCLUDE_PARAMS" != "./" ]; then
+	rsync -r $DIR/* $SANDBOX_HOME/$DIRECTORY_WITHIN $EXCLUDE_PARAMS
+fi
 
 Mount
 
-firejail $FIREJAIL_ARGS --noblacklist=~/.gradle --private-tmp --private=$SANDBOX_HOME bash -c  "cd ~/$DIRECTORY_WITHIN/ && $COMMAND"
+FIREJAIL_UNBLACKLIST_ARGS=$(for i in $(echo "$MUTABLE_RELATIVE_TO_HOME" | sed "s/,/\n/g")
+do
+	printf " --noblacklist=~/$i "
+done)
+
+firejail $FIREJAIL_ARGS $FIREJAIL_UNBLACKLIST_ARGS --private=$SANDBOX_HOME bash -c  "cd ~/$DIRECTORY_WITHIN/ && $COMMAND"
 
 Umount
+AbortIfMounted
 
 rm -rf $SANDBOX_HOME/$DIRECTORY_WITHIN
 
