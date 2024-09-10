4754e8cf121e6012698724757bc8810f31a6bce2
Niki <nightfallout@proton.me>
2024-09-10 03:43:16 +0000

    system [environments/fabricmc]: added kotlin-language-server

--- system/environments/fabricmc.nix
+++ system/environments/fabricmc.nix
@@ -1,5 +1,11 @@
 { pkgs ? import <nixpkgs> {} }: pkgs.mkShell {
-  packages = with pkgs; [ deno gradle jdk21 jdt-language-server ];
+  packages = with pkgs; [
+    jdk21
+    jdt-language-server
+    kotlin-language-server
+    gradle
+    deno
+  ];
 
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
     libpulseaudio
