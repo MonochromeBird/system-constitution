b568389d4ea197dc74971e9c56bf44525efc8d5a
Niki <nightfallout@proton.me>
2024-09-15 19:21:23 +0000

    system [modules/programming/godot]: added 4.4-dev2 as godot4-mono-latest

--- system/modules/programming/godot-mono-derivation.nix
+++ system/modules/programming/godot-mono-derivation.nix
@@ -2,8 +2,14 @@
 # I couldn't get the derivation in the PR to build, so I adapted it
 # to just fetch the binaries directly.
 
-{ pkgs ? import <nixpkgs> {} }: pkgs.stdenv.mkDerivation rec {
-  version = "v4.3-stable";
+{
+  pkgs ? import <nixpkgs> {},
+  version ? "v4.3-stable",
+  url_version ? "",
+  bin_name ? "godot4-mono",
+  repo ? "godot",
+  hash ? "sha256-7N881aYASmVowZlYHVi6aFqZBZJuUWd5BrdvvdnK01E="
+}: pkgs.stdenv.mkDerivation rec {
   release = "Godot_${version}_mono_linux";
   bin = "${release}.x86_64";
 
@@ -11,11 +17,11 @@
   zipname = "${dirname}.zip";
 
   name = "Godot_${version}_mono";
-  pname  = "godot4-mono";
+  pname = bin_name;
   
   src = pkgs.fetchurl {
-    url = "https://github.com/godotengine/godot/releases/download/${version}/${zipname}";
-    hash = "sha256-7N881aYASmVowZlYHVi6aFqZBZJuUWd5BrdvvdnK01E=";
+    url = "https://github.com/godotengine/${repo}/releases/download/${if url_version == "" then version else url_version}/${zipname}";
+    hash = hash;
   };
 
   nativeBuildInputs = with pkgs; [
--- system/modules/programming/godot.nix
+++ system/modules/programming/godot.nix
@@ -2,6 +2,15 @@
 {
 	packages = with pkgs; [
 		godot_4
+		
 		(pkgs.callPackage ./godot-mono-derivation.nix {})
+		
+		(pkgs.callPackage ./godot-mono-derivation.nix {
+			bin_name = "godot4-mono-latest";
+			repo = "godot-builds";
+			version = "v4.4-dev2";
+			url_version = "4.4-dev2";
+			hash = "sha256-/d+lRbUSG/WIpH+EXNfiA+H6bSWQlRtjdyBZ6XWP9bM=";
+		})
 	];
 }
