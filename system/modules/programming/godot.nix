b59bd3586bbded0c699ee50d5095c73c13ddd541
anonymalek <alekthesixth@protonmail.com>
2024-10-03 20:13:40 -0300

    module [godot]: updated to dev3

--- system/modules/programming/godot.nix
+++ system/modules/programming/godot.nix
@@ -8,8 +8,8 @@
 		(pkgs.callPackage ./godot-mono-derivation.nix {
 			bin_name = "godot4-mono-latest";
 			repo = "godot-builds";
-			version = "4.4-dev2";
-			hash = "sha256-/d+lRbUSG/WIpH+EXNfiA+H6bSWQlRtjdyBZ6XWP9bM=";
+			version = "4.4-dev3";
+			hash = "sha256-K9AWkLnWCyIXPkFUkdAJbJuldrrrOX/8Ysun2iIdelI=";
 		})
 	];
 }
