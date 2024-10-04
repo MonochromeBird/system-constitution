198417c4f64a66bf4d90947a060494123d233b3c
anonymalek <alekthesixth@protonmail.com>
2024-10-04 00:44:19 -0300

    system [godot]: added back 4.4 dev 2

--- system/modules/programming/godot.nix
+++ system/modules/programming/godot.nix
@@ -11,5 +11,12 @@
 			version = "4.4-dev3";
 			hash = "sha256-K9AWkLnWCyIXPkFUkdAJbJuldrrrOX/8Ysun2iIdelI=";
 		})
+
+		(pkgs.callPackage ./godot-mono-derivation.nix {
+			bin_name = "godot4-mono-4.4dev2";
+			repo = "godot-builds";
+			version = "4.4-dev2";
+			hash = "sha256-/d+lRbUSG/WIpH+EXNfiA+H6bSWQlRtjdyBZ6XWP9bM=";
+		})
 	];
 }
