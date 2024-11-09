4ebea511e5a52e01fa3ea603843a306672cf0c0d
anonymalek <alekthesixth@protonmail.com>
2024-11-08 21:13:03 -0300

    system [godot]: update latest to 4.4dev4

--- system/modules/programming/godot.nix
+++ system/modules/programming/godot.nix
@@ -8,8 +8,8 @@
 		(pkgs.callPackage ./godot-mono-derivation.nix {
 			bin_name = "godot4-mono-latest";
 			repo = "godot-builds";
-			version = "4.4-dev3";
-			hash = "sha256-K9AWkLnWCyIXPkFUkdAJbJuldrrrOX/8Ysun2iIdelI=";
+			version = "4.4-dev4";
+			hash = "sha256-QFzif3IGwbPkSrvhEnnRs8m38NmooZoOG8FVAbg6L6k=";
 		})
 
 		(pkgs.callPackage ./godot-mono-derivation.nix {
