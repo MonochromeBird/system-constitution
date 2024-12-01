c5227a3051dcd380c09577d2b3aa7c5058f11f48
anonymalek <alekthesixth@protonmail.com>
2024-12-01 14:36:31 -0300

    system [pipewire]: fix choppy audio

--- system/modules/audio/pipewire.nix
+++ system/modules/audio/pipewire.nix
@@ -12,5 +12,14 @@
 		# may enable pulseaudio by default, and it conflicts
 		# with pipewire.
 		hardware.pulseaudio.enable = false;
+
+		services.pipewire.extraConfig.pipewire."92-low-latency" = {
+			"context.properties" = {
+    			"default.clock.rate" = 48000;
+				"default.clock.quantum" = 512;
+				"default.clock.min-quantum" = 512;
+				"default.clock.max-quantum" = 512;
+			};
+		};
 	};
 }
