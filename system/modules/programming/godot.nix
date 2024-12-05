c25a924c605d1aaf41cff92d3e94af577266a88c
anonymalek <alekthesixth@protonmail.com>
2024-12-05 18:20:04 -0300

    system [godot]: updated to dev6

--- system/modules/programming/godot.nix
+++ system/modules/programming/godot.nix
@@ -8,15 +8,8 @@
 		(pkgs.callPackage ./godot-mono-derivation.nix {
 			bin_name = "godot4-mono-latest";
 			repo = "godot-builds";
-			version = "4.4-dev5";
-			hash = "sha256-rG1Weuv02NZPDKnK/KrvBdYqnWCBkTHYCqtB/l91+zE=";
-		})
-
-		(pkgs.callPackage ./godot-mono-derivation.nix {
-			bin_name = "godot4-mono-4.4dev2";
-			repo = "godot-builds";
-			version = "4.4-dev2";
-			hash = "sha256-/d+lRbUSG/WIpH+EXNfiA+H6bSWQlRtjdyBZ6XWP9bM=";
+			version = "4.4-dev6";
+			hash = "sha256-RaexKl+2/QfqIS+US/g+IAQdtWP6BbWsyMgIY67uT1k=";
 		})
 	];
 }
