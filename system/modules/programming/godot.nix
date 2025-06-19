b782f93cd852c7fb8c8516d1c39caf63e7248356
Niki <nightfallout@proton.me>
2025-06-19 17:04:40 +0000

    system [modules/programming/godot]: godot4-mono-latest: updated to 4.5-beta1

--- system/modules/programming/godot.nix
+++ system/modules/programming/godot.nix
@@ -8,8 +8,8 @@
 		(pkgs.callPackage ./godot-mono-derivation.nix {
 			bin_name = "godot4-mono-latest";
 			repo = "godot-builds";
-			version = "4.5-dev3";
-			hash = "sha256-Emp453yqSFQCEMuXURZzKittL8L02rgxIHXg0QVuA6Q=";
+			version = "4.5-beta1";
+			hash = "sha256-JQShoa4EgVaxcSCjuR0QifFuJlg8guTsg3avPvv9iHQ=";
 		})
 	];
 }
