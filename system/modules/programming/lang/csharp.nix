cec77a20a30e0e2d4613a5067893419789c06aba
Niki <nightfallout@proton.me>
2025-07-12 15:09:16 +0000

    nix flake update (pinned dotnet-sdk_10 to older nixpkgs)

--- system/modules/programming/lang/csharp.nix
+++ system/modules/programming/lang/csharp.nix
@@ -1,7 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, system, ... }:
 {
 	packages = with pkgs; [
-		dotnet-sdk_10 nuget
+    (import (pkgs.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "30e2e2857ba47844aa71991daa6ed1fc678bcbb7";
+      sha256 = "1hw4ccgwzi120j41xr8k8njsxk74l0ciir1g93mij1p43cmrgccj";
+    }) { inherit system; }).dotnet-sdk_10
+
+		nuget
 		omnisharp-roslyn
 		netcoredbg
 	];
